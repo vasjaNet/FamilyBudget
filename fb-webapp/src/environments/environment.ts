9129b0353d51f005cf9a6b12dcdc7574a4e95bf3
vasyl <vasjasynenko@gmail.com>
2026-03-20 21:40:47 +0200

    FB-6 : Add fb-webapp as a subproject

--- fb-webapp/src/environments/environment.ts
+++ fb-webapp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   /** Use '' with proxy (ng serve) so /api is forwarded to backend; or set to http://localhost:8080 for direct calls */
-  apiBaseUrl: '',
+  apiBaseUrl: 'http://localhost:8080',
   apiPrefix: '/api/v1',
 };
