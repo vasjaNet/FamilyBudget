0ff2fce8bc9120a94e9d422d3af18618e66581a0
vasyl <vasjasynenko@gmail.com>
2026-03-27 08:30:59 +0200

    FB-10 : Refactor Pipeline, add SonarCloud analysis & ESLint check

---
+++ fb-webapp/eslint.config.js
@@ -0,0 +1,82 @@
+/** @type {import('eslint').Linter.Config[]} */
+module.exports = [
+  // Base configuration for TypeScript files
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-empty-function': 'warn',
+    },
+  },
+  // Angular specific rules for TypeScript files
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      '@angular-eslint': require('@angular-eslint/eslint-plugin'),
+    },
+    rules: {
+      '@angular-eslint/component-class-suffix': 'error',
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      '@angular-eslint/directive-class-suffix': 'error',
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/no-empty-lifecycle-method': 'error',
+      '@angular-eslint/no-output-native': 'error',
+      '@angular-eslint/no-output-on-prefix': 'error',
+      '@angular-eslint/use-lifecycle-interface': 'error',
+      '@angular-eslint/use-pipe-transform-interface': 'error',
+    },
+  },
+  // Angular template rules for HTML files
+  {
+    files: ['**/*.html'],
+    plugins: {
+      '@angular-eslint/template': require('@angular-eslint/eslint-plugin-template'),
+    },
+    rules: {
+      '@angular-eslint/template/accessibility-alt-text': 'error',
+      '@angular-eslint/template/accessibility-elements-content': 'error',
+      '@angular-eslint/template/accessibility-label-has-associated-control': 'error',
+      '@angular-eslint/template/accessibility-valid-aria': 'error',
+      '@angular-eslint/template/click-events-have-key-events': 'error',
+      '@angular-eslint/template/mouse-events-have-key-events': 'error',
+      '@angular-eslint/template/no-autofocus': 'error',
+      '@angular-eslint/template/no-distracting-elements': 'error',
+      '@angular-eslint/template/no-duplicate-attributes': 'error',
+      '@angular-eslint/template/no-positive-tabindex': 'error',
+    },
+  },
+  // Ignore patterns
+  {
+    ignores: [
+      'dist/',
+      'node_modules/',
+      '.angular/',
+      'coverage/',
+    ],
+  },
+];
